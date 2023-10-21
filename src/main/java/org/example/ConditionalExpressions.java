dc526f88169301283fbdfa0555429051455a3330
Rando <Randomitt@gmail.com>
2023-10-21 13:58:14 +0300

    Some comment about case sensitivve ids added

---
+++ src/main/java/org/example/ConditionalExpressions.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class ConditionalExpressions {
+
+    public static void main(String[] args) {
+        System.out.println("Conditional expressions and operators");
+
+        boolean condition = false;
+        int age = 17;
+
+        boolean isAdult = age >= 18;
+        boolean isKid = age < 18;
+
+        System.out.println("Is adult: " + isAdult);
+        System.out.println("Is kid: " + isKid);
+    }
+}
