f085c1c157115a27012e8f1024b5ff2f8a173c5b
Rando <Randomitt@gmail.com>
2023-10-21 12:31:05 +0300

    Some comment about case sensitivve ids added

---
+++ src/main/java/org/example/MyFirstApp.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//use Code -> Refactor to reformat source code
+public class MyFirstApp {
+
+    // psvm
+    public static void main(String[] args) {
+
+        // sout- shortcut
+        //või kasuta ctrl+cpacebar
+        System.out.println(555);
+        System.out.println("5 5 5 ");
+    }
+
+}
