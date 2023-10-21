9bb68235fe95586aaae0130655e305643af92dd0
Rando <Randomitt@gmail.com>
2023-10-21 11:31:37 +0300

    My first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // some useful shortcut
+        //line duplication
+        //ctrl+D
+        System.out.println("Hello world!");
+        //System.out.println("YES: I DID IT!");
+        //System.out.println("Hello RANDO!");
+    }
+}
