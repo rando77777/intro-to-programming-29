afe0f31a16a703be91fdae6f2581313cf4789b9e
Rando <Randomitt@gmail.com>
2023-10-21 13:49:35 +0300

    Some comment about case sensitivve ids added

--- src/main/java/org/example/JavaTypes.java
+++ src/main/java/org/example/JavaTypes.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class JavaTypes {
+
     public static void main(String[] args) {
         System.out.println("Java types example");
         System.out.println("Numbers first");
@@ -29,6 +30,60 @@ public class JavaTypes {
 
 //        int class = 5;
 
+        System.out.println("Let's meet numbers type");
+        System.out.println("int type");
+        System.out.println("Smallest int is: " + Integer.MIN_VALUE);
+        System.out.println("Biggest int is: " + Integer.MAX_VALUE);
 
+        System.out.println();
+
+        System.out.println("long type");
+        long longAge = 5;
+        System.out.println("Smallest long is: " + Long.MIN_VALUE);
+        System.out.println("Biggest long is: " + Long.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("byte type");
+        byte byteAge = 5;
+        System.out.println("Smallest byte is: " + Byte.MIN_VALUE);
+        System.out.println("Biggest byte is: " + Byte.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("short type");
+        short shortAge = 5;
+        System.out.println("Smallest short is: " + Short.MIN_VALUE);
+        System.out.println("Biggest short is: " + Short.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("double type");
+        double doubleAge = 1.5;
+        System.out.println("Smallest double is: " + Double.MIN_VALUE);
+        System.out.println("Biggest double is: " + Double.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("float type");
+        float floatAge = 1.5f;
+        System.out.println("Smallest float is: " + Float.MIN_VALUE);
+        System.out.println("Biggest float is: " + Float.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("char type");
+        char singleLetter = 'a'; // "ddd"
+        // TODO: explain range
+
+        System.out.println();
+
+        System.out.println("boolean type");
+        // true or false
+        boolean trueValue = true;
+        boolean falseValue = false;
+
+        System.out.println("trueValue: " + trueValue);
+        System.out.println("falseValue: " + falseValue);
     }
 }
