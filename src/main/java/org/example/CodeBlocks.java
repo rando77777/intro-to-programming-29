96eda0f4c75ddffb18774309aa8e55d868862784
Rando <Randomitt@gmail.com>
2023-10-22 10:20:05 +0300

    Some comment about case sensitivve ids added

--- src/main/java/org/example/CodeBlocks.java
+++ src/main/java/org/example/CodeBlocks.java
@@ -2,20 +2,29 @@ package org.example;
 
 public class CodeBlocks {
 
-    //main is function - feature
+    // main is function - feature
     public static void main(String[] args) {
 
-        if (true); //empty instruction
+        if (true) ; // empty instruction
 
         if (true) {
             System.out.println("first");
             System.out.println("second");
-
         }
+
         if (false) {
+            System.out.println("third");
+            System.out.println("third");
+            System.out.println("third");
+            System.out.println("third");
+            System.out.println("third");
             System.out.println("third");
         }
 
         System.out.println("fourth");
+
+        if (true) {
+            System.out.println("Just true");
+        }
     }
 }
