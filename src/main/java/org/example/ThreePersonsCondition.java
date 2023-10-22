ecc94556757dca3891fc1928b88949afa78d3d97
Rando <Randomitt@gmail.com>
2023-10-22 10:36:24 +0300

    threee person example- without functions

---
+++ src/main/java/org/example/ThreePersonsCondition.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class ThreePersonsCondition {
+
+    public static void main(String[] args) {
+        int age = 55;
+
+        if (age >= 18) {
+            System.out.println("If is true");
+            System.out.println("Adult");
+        } else {
+            System.out.println("Else, if was false");
+            System.out.println("Kid");
+        }
+
+        int johnAge = 5;
+        if (johnAge >= 18) {
+            System.out.println("If is true");
+            System.out.println("Adult");
+        } else {
+            System.out.println("Else, if was false");
+            System.out.println("Kid");
+        }
+
+        int marthaAge = 19;
+        if (marthaAge >= 18) {
+            System.out.println("If is true");
+            System.out.println("Adult");
+        } else {
+            System.out.println("Else, if was false");
+            System.out.println("Kid");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
