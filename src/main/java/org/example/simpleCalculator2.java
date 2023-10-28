fb694e1b3e9185f5af2808f7bbebade47f8f0799
Rando <Randomitt@gmail.com>
2023-10-28 09:31:21 +0300

    Homwork calculator

---
+++ src/main/java/org/example/simpleCalculator2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class simpleCalculator2 {
+
+    public static void main(String[] args) {
+        System.out.println("Starting calculations");
+
+        addThreeNumbers(1, 2, 3);
+        addThreeNumbers(25, 17, 10);
+        addThreeNumbers(255, 17, 13);
+
+        int first = 5;
+        int second = 6;
+        int third =7
+        addThreeNumbers(first, second);
+        addThreeNumbers(first, first);
+        addThreeNumbers(first, second, third);
+    }
+
+    private static void addThreeNumbers(int 10, int 12, int 13) {
+    }
+
+    public static void addTwoNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        // First number is: 1, second number is: 5, third number 7
+        System.out.println("First number is: " + firstNumber + ", second number is: " + secondNumber);
+        System.out.println("first number is: " +);
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum of three numbers is: " + sum);
+    }
+}
