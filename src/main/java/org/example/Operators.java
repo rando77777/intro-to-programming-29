4d0c9f15964cc99426d40eee63327bab84167ed7
Rando <Randomitt@gmail.com>
2023-10-28 09:50:06 +0300

    Java operators - part 1

---
+++ src/main/java/org/example/Operators.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Java operators");
+
+        // compare operators
+        // equals operator ==
+        // not equal !=
+        int a = 5;
+        int b = 6;
+        boolean isAEqualToB = a == b;
+        System.out.println("Is a equals to b?: " + isAEqualToB);
+    }
+}
+
+
+
+
+
+
+
+
+
